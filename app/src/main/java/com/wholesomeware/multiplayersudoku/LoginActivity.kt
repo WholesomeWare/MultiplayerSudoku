18690c9f9bf4412363be89a8f32a7892fd4af30b
Viktória Kerecsényi <viktoriakerecsenyi@gmail.com>
2024-03-01 21:38:02 +0100

    Bejelentkező felület

--- app/src/main/java/com/wholesomeware/multiplayersudoku/LoginActivity.kt
+++ app/src/main/java/com/wholesomeware/multiplayersudoku/LoginActivity.kt
@@ -3,8 +3,14 @@ package com.wholesomeware.multiplayersudoku
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,8 +18,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wholesomeware.multiplayersudoku.ui.theme.MultiplayerSudokuTheme
 
 class LoginActivity : ComponentActivity() {
@@ -31,13 +40,52 @@ class LoginActivity : ComponentActivity() {
             var email by remember { mutableStateOf("") }
             var password by remember { mutableStateOf("") }
 
+            //TODO: Bejelentkezési felület. Csak hogy meglegyen a kellő komplexitás,
+            // legyen email-jelszó kombós és Google bejelentkezés.
+            // Ezekhez összerakom a backend-et, próbálj csinálni valami UI-t neki.
             Surface(
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
-                //TODO: Bejelentkezési felület. Csak hogy meglegyen a kellő komplexitás,
-                // legyen email-jelszó kombós és Google bejelentkezés.
-                // Ezekhez összerakom a backend-et, próbálj csinálni valami UI-t neki.
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    OutlinedTextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        modifier = Modifier.fillMaxWidth()
+                            .padding(bottom = 16.dp),
+                        label = { Text("Email") }
+                    )
+
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        modifier = Modifier.fillMaxWidth()
+                            .padding(bottom = 16.dp),
+                        label = { Text("Password") },
+                        visualTransformation = PasswordVisualTransformation(),
+                    )
+
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text("Bejelentkezés")
+                    }
+
+
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text("Google bejelentkezés")
+                    }
+                }
             }
         }
     }
