1fb5b63f8e00272d13c630eda4d2c7f57b9ef31e
Viktória Kerecsényi <viktoriakerecsenyi@gmail.com>
2024-03-07 14:20:00 +0100

    Menü

--- app/src/main/java/com/wholesomeware/multiplayersudoku/MainActivity.kt
+++ app/src/main/java/com/wholesomeware/multiplayersudoku/MainActivity.kt
@@ -11,18 +11,24 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ConnectWithoutContact
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -31,6 +37,7 @@ import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -42,6 +49,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -82,7 +90,7 @@ class MainActivity : ComponentActivity() {
                 Column {
                     CenterAlignedTopAppBar(
                         title = {
-                            Text(text = "Sudoku")
+                            Text(text = "Multiplayer Sudoku")
                         },
                         navigationIcon = {
                             //TODO: app icon
@@ -115,48 +123,71 @@ class MainActivity : ComponentActivity() {
                     verticalArrangement = Arrangement.Center,
                     modifier = Modifier.fillMaxSize(),
                 ) {
-                    Row {
                         ElevatedCard(
                             colors = CardDefaults.cardColors(
-                                containerColor = MaterialTheme.colorScheme.surface,
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
                             ),
                             modifier = Modifier
-                                .size(width = 180.dp, height = 200.dp)
+                                .height(170.dp)
+                                .padding(5.dp)
                         ) {
                             Column(
-                                horizontalAlignment = Alignment.CenterHorizontally,
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .padding(top = 10.dp)
+                                modifier = Modifier.fillMaxSize()
                             ) {
-                                Text(text = "Új szoba")
+                                Text(
+                                    text = "Új szoba",
+                                    modifier = Modifier.padding(16.dp),
+                                    fontSize = 25.sp,
+                                    fontWeight = FontWeight.Bold,
+                                )
+                                Text(
+                                    text = "Még valami",
+                                    modifier = Modifier.padding(start = 16.dp),
+                                    fontSize = 15.sp,
+                                )
                                 Spacer(modifier = Modifier.weight(1f))
-                                OutlinedButton(
-                                    onClick = { /*TODO*/ }) {
-                                    Text("Létrehozás")
+                                Row(
+                                    horizontalArrangement = Arrangement.End,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(end = 16.dp)
+                                ) {
+                                    Button(
+                                        onClick = { /*TODO*/ },
+                                        modifier = Modifier.padding(end = 16.dp, bottom = 16.dp)
+                                    ) {
+                                        Icon(
+                                            Icons.Filled.Add,
+                                            contentDescription = "Csatlakozás ikon"
+                                        )
+                                        Text(
+                                            "Létrehozás",
+                                            modifier = Modifier.padding(6.dp),
+                                        )
+                                    }
                                 }
+
                             }
                         }
-
-                        Spacer(modifier = Modifier.width(16.dp))
-
                         ElevatedCard(
                             colors = CardDefaults.cardColors(
-                                containerColor = MaterialTheme.colorScheme.surface,
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
                             ),
                             modifier = Modifier
-                                .size(width = 180.dp, height = 200.dp)
+                                .height(200.dp)
+                                .padding(5.dp)
                         ) {
                             Column(
-                                horizontalAlignment = Alignment.CenterHorizontally,
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .padding(top = 10.dp)
+                                modifier = Modifier.fillMaxSize()
                             ) {
-                                Text(text = "Csatlakozás szobához")
+                                Text(text = "Csatlakozás szobához",
+                                    modifier = Modifier.padding(16.dp),
+                                    fontSize = 25.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    )
                                 var text by rememberSaveable { mutableStateOf("") }
 
-                                OutlinedTextField(
+                                TextField(
                                     value = text,
                                     onValueChange = { text = it },
                                     placeholder = {
@@ -166,24 +197,32 @@ class MainActivity : ComponentActivity() {
                                         )
                                     },
                                     modifier = Modifier
-                                        .width(150.dp)
-                                        .height(60.dp)
-                                        .padding(top = 10.dp),
+                                        .fillMaxWidth()
+                                        .padding(start = 16.dp, end = 16.dp)
+                                        .height(50.dp),
                                     textStyle = TextStyle(fontSize = 14.sp)
 
                                 )
                                 Spacer(modifier = Modifier.weight(1f))
-                                OutlinedButton(onClick = { /*TODO*/ }) {
-                                    Text("Csatlakozás")
+                                Row(
+                                    horizontalArrangement = Arrangement.End,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(end = 16.dp)
+                                ){
+                                    Button(onClick = { /*TODO*/ },
+                                        modifier = Modifier.padding(end = 16.dp, bottom = 16.dp)) {
+                                        Icon(Icons.Filled.ConnectWithoutContact, contentDescription = "Csatlakozás ikon")
+                                        Text("Csatlakozás",
+                                            modifier = Modifier.padding(6.dp),)
+                                    }
                                 }
+
                             }
                         }
-
-                    }
                 }
 
             }
         }
     }
 }
-
