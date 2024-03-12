31658f3aa366c43cb27d2eb46965aa4112614dc9
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2024-03-12 17:05:42 +0100

    Pár alap dolog a LobbyActivity-hez

--- app/src/main/java/com/wholesomeware/multiplayersudoku/LobbyActivity.kt
+++ app/src/main/java/com/wholesomeware/multiplayersudoku/LobbyActivity.kt
@@ -3,11 +3,15 @@ package com.wholesomeware.multiplayersudoku
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -15,7 +19,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.wholesomeware.multiplayersudoku.firebase.Auth
+import com.wholesomeware.multiplayersudoku.firebase.Firestore
+import com.wholesomeware.multiplayersudoku.model.Player
 import com.wholesomeware.multiplayersudoku.model.Room
+import com.wholesomeware.multiplayersudoku.sudoku.Sudoku
 import com.wholesomeware.multiplayersudoku.ui.theme.MultiplayerSudokuTheme
 
 class LobbyActivity : ComponentActivity() {
@@ -31,6 +38,19 @@ class LobbyActivity : ComponentActivity() {
     private fun LobbyScreen() {
         MultiplayerSudokuTheme {
             var room by remember { mutableStateOf(Room()) }
+            val isOwner by remember(room) {
+                mutableStateOf(room.ownerId == Auth.getCurrentUser()?.uid)
+            }
+            var players by remember { mutableStateOf(emptyList<Player>()) }
+            var selectedDifficulty by remember { mutableStateOf(Sudoku.Difficulty.EASY) }
+
+            // Ez az effect akkor fut le, amikor a szobában valami megváltozik.
+            LaunchedEffect(room) {
+                // Lekérjük a játékosokat id alapján, mert a `room` csak az id-jüket tárolja
+                Firestore.Players.getPlayersByIds(room.players) {
+                    players = it
+                }
+            }
 
             //TODO: játékos lista, kezdés gomb és nehézség állítás a tulajnak
 
@@ -38,10 +58,23 @@ class LobbyActivity : ComponentActivity() {
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
-                Text(
-                    text = if (room.ownerId == Auth.getCurrentUser()?.uid) "Tulaj vagyok"
-                    else "Vendég vagyok"
-                )
+                Column {
+                    Text(
+                        text = if (isOwner) "Tulaj vagyok"
+                        else "Vendég vagyok"
+                    )
+                    Column(
+                        modifier = Modifier.verticalScroll(rememberScrollState())
+                    ) {
+                        // Ha meg akarjuk jeleníteni az összes játékost akkor egy ciklussal simán végigmehetünk rajtuk.
+                        players.forEach { player ->
+                            // Ide kell megadni, hogy hogyan nézzen ki 1 játékos.
+                            // Az itteni UI elemek fognak ismétlődni játékosonként.
+                        }
+                    }
+                    // Játékosok alá egy meghívás gomb, ami intent-el megosztja a meghívó kódot
+                    // minden appnak, ami tud szöveget fogadni.
+                }
             }
         }
     }
