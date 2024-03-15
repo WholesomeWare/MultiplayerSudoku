fde655b81b34acc6febaae7b9fe86967fc5ceac5
Viktória Kerecsényi <viktoriakerecsenyi@gmail.com>
2024-03-15 07:23:39 +0100

    Update LobbyActivity.kt

--- app/src/main/java/com/wholesomeware/multiplayersudoku/LobbyActivity.kt
+++ app/src/main/java/com/wholesomeware/multiplayersudoku/LobbyActivity.kt
@@ -3,21 +3,48 @@ package com.wholesomeware.multiplayersudoku
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Start
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wholesomeware.multiplayersudoku.firebase.Auth
 import com.wholesomeware.multiplayersudoku.firebase.Firestore
 import com.wholesomeware.multiplayersudoku.model.Player
@@ -33,6 +60,7 @@ class LobbyActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Preview
     @Composable
     private fun LobbyScreen() {
@@ -63,17 +91,74 @@ class LobbyActivity : ComponentActivity() {
                         text = if (isOwner) "Tulaj vagyok"
                         else "Vendég vagyok"
                     )
+                    CenterAlignedTopAppBar(
+                        title = {
+                            Text(text = "X szobája")
+                        },
+                        navigationIcon = {
+                            IconButton(onClick = { /* do something */ }) {
+                                Icon(
+                                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                    contentDescription = null,
+                                )
+                            }
+                        },
+                    )
+                    ElevatedCard(
+                        colors = CardDefaults.cardColors(
+                            containerColor = Color.Gray,
+                        ),
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        Text(
+                            text = "Hívd meg a barátaidat, és játszatok együtt",
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
+
+
+
                     Column(
                         modifier = Modifier.verticalScroll(rememberScrollState())
                     ) {
                         // Ha meg akarjuk jeleníteni az összes játékost akkor egy ciklussal simán végigmehetünk rajtuk.
                         players.forEach { player ->
+                            Row {
+                                Icon(
+                                    imageVector = Icons.Default.AccountCircle,
+                                    contentDescription = null,
+                                )
+                                Text(text = player.name)
+                            }
+
                             // Ide kell megadni, hogy hogyan nézzen ki 1 játékos.
                             // Az itteni UI elemek fognak ismétlődni játékosonként.
                         }
                     }
+
                     // Játékosok alá egy meghívás gomb, ami intent-el megosztja a meghívó kódot
                     // minden appnak, ami tud szöveget fogadni.
+
+                    BottomAppBar(
+
+                        actions = {
+                                  Text(text = "valami")
+                        },
+                        floatingActionButton = {
+                            ExtendedFloatingActionButton(
+                                onClick = { /*TODO*/ },
+
+                            ){
+                                Icon(
+                                    Icons.Filled.PlayArrow,
+                                    contentDescription = "Csatlakozás ikon"
+                                )
+                                Text(text = "Indítás")
+                            }
+
+                        }
+
+                    )
                 }
             }
         }
