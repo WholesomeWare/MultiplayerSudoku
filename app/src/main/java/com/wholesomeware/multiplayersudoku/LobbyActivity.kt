9202689a04cea5107630f1957f2d9a9500998120
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2024-03-15 22:58:20 +0100

    Translated difficulties

--- app/src/main/java/com/wholesomeware/multiplayersudoku/LobbyActivity.kt
+++ app/src/main/java/com/wholesomeware/multiplayersudoku/LobbyActivity.kt
@@ -43,6 +43,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.firebase.firestore.ListenerRegistration
@@ -250,7 +251,9 @@ class LobbyActivity : ComponentActivity() {
                                                     room.copy(difficultyId = difficulty.ordinal)
                                                 ) {}
                                             },
-                                            text = { Text(text = difficulty.name) }
+                                            text = {
+                                                Text(text = stringResource(id = difficulty.stringResourceId))
+                                            }
                                         )
                                     }
                                 }
