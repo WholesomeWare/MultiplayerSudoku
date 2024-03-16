391169ffb00eef2fa6fe037291cf65b646b7d560
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2024-03-16 18:46:39 +0100

    isStarted not updating fixed

--- app/src/main/java/com/wholesomeware/multiplayersudoku/model/Room.kt
+++ app/src/main/java/com/wholesomeware/multiplayersudoku/model/Room.kt
@@ -5,6 +5,7 @@ data class Room(
     val players: List<String> = emptyList(),
     val ownerId: String = "",
     val difficultyId: Int = 0,
+    @JvmField
     val isStarted: Boolean = false,
     val sudoku: SerializableSudoku = SerializableSudoku(),
     val startTime: Long = 0,
