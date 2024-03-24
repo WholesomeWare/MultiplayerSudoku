5bd51f0c845e5ce69722797f1b54cf51bf1d5a58
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2024-03-24 11:24:42 +0100

    0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.wholesomeware.multiplayersudoku"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
